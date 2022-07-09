4fe9679de0125e7e82c2c90bfb9da12152529840
houssamOuriarhi <ouriarhi.houssam@gmail.com>
2022-07-09 20:11:56 +0100

    Add project files

---
+++ app.js
@@ -0,0 +1,18 @@
+// Book Class: Represent a Book
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+// UI Class: Handle UI Tasks
+
+// Store Class: Handles storage
+
+//Event: Display Books
+
+//Event: Add a Book
+
+//Event: Remove a Book
